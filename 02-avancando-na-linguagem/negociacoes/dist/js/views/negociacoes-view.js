d76f902bafd7026242971da106b1093bd3d340c8
Henio Junior <henioajunior@gmail.com>
2023-02-17 05:57:09 -0300

    Removendo comentários do código

--- 02-avancando-na-linguagem/negociacoes/dist/js/views/negociacoes-view.js
+++ 02-avancando-na-linguagem/negociacoes/dist/js/views/negociacoes-view.js
@@ -25,7 +25,6 @@ export class NegociacoesView extends View {
         `;
     }
     formatarData(data) {
-        //return new Intl.DateTimeFormat('pt-BR').format(data)
         return new Intl.DateTimeFormat().format(data);
     }
 }
