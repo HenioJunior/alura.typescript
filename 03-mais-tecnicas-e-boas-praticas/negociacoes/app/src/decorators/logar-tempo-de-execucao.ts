ca28b2df74eee2cfc017e8fc4145863833422cc6
Henio Junior <henioajunior@gmail.com>
2023-02-18 08:44:35 -0300

    Finalizado o decorator inspect

--- 03-mais-tecnicas-e-boas-praticas/negociacoes/app/src/decorators/logar-tempo-de-execucao.ts
+++ 03-mais-tecnicas-e-boas-praticas/negociacoes/app/src/decorators/logar-tempo-de-execucao.ts
@@ -7,18 +7,26 @@ export function logarTempoDeExecucao(emSegundos: boolean = false) {
         const metodoOriginal = descriptor.value;
         
         descriptor.value = function(...args: any[]) {
+            
+            console.log("+++ Logar Tempo De Execução +++")
+
             let divisor = 1;
+            
             let unidade = 'milisegundos';
+            
             if(emSegundos) {
                 unidade = 'segundos'
                 divisor = 1000;
             }
 
             const t1 = performance.now();
+            
             const retorno = metodoOriginal.apply(this, args);
+            
             const t2 = performance.now();
-
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}.`);
+            
+            console.log(`------ Método: ${propertyKey}, Tempo de execução: ${(t2 - t1) / divisor} ${unidade}.`);
+            
             retorno
         };
 
